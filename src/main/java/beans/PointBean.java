e3a3e7f51d213c56bd8ca745f0e55d18b497839e
vlktna <59750615+vlktna@users.noreply.github.com>
2020-12-06 21:27:16 +0300

    update bean

--- src/main/java/beans/PointBean.java
+++ src/main/java/beans/PointBean.java
@@ -1,1 +1,58 @@
+package beans;
+
+import dao.JpaPointDao;
 import entity.Point;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class PointBean implements Serializable {
+
+    private Point point;
+    private List<Point> pointsList;
+    private JpaPointDao service;
+
+    @PostConstruct
+    public void postConstruct(){
+        point = new Point();
+        point.setValueR(2.25);
+        pointsList = new ArrayList<>();
+        service = new JpaPointDao();
+        service.createEntityManager();
+    }
+
+    public void addPoint(){
+        point.setTime(String.valueOf(new Date()));
+        point.setResult(String.valueOf(point.calculateResult()));
+        pointsList.add(point);
+        service.save(point);
+        point = new Point();
+    }
+
+    public void clear(){
+        pointsList = new ArrayList<>();
+        service.deleteAll();
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+
+    public List<Point> getPointsList() {
+        return pointsList;
+    }
+
+    public void setPointsList(List<Point> pointsList) {
+        this.pointsList = pointsList;
+    }
+}
